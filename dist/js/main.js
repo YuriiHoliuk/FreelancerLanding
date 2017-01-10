8f90de8eefc7f148c024d1a3ff2d12fdf4e48155
YuriiHoliuk <bionicuss@gmail.com>
2017-01-10 15:32:22 +0200

    gulp build check

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 
 	var $header = $("#header");
+	var width = $(window).width();
+	var scroll = 300;
+
+	if ((width < 768) && (width > 570)) {
+		scroll = 200;
+	} else if (width <= 570) {
+		scroll = 100;
+	}
 
 	$(window).scroll(function() {
 
-		if ( $(this).scrollTop() > 600 && !$header.hasClass("js-fixed-geader") ) {
+		if ( $(this).scrollTop() > scroll && !$header.hasClass("js-fixed-geader") ) {
 			$header.addClass("js-fixed-header");
-		} else if ( $(this).scrollTop() <= 600 && $header.hasClass("js-fixed-header") ) {
+		} else if ( $(this).scrollTop() <= scroll && $header.hasClass("js-fixed-header") ) {
 			$header.removeClass("js-fixed-header");
 		}
 	})
